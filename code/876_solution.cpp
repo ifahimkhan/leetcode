74f8e4785ea5bb3a3d2c21088d6e03c701c07f9f
ren <ryanzjlib@gmail.com>
2020-05-28 11:46:26 -0400

    update a one pass to 876

--- code/876_solution.cpp
+++ code/876_solution.cpp
@@ -9,11 +9,20 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode *node = head;
-        int n;
-        for (n=0;node;n++) node=node->next;
-        node = head;
-        for (int i=0;i<n/2;i++) node=node->next;
-        return node;
+        // two pass
+        // ListNode *node = head;
+        // int n;
+        // for (n=0;node;n++) node=node->next;
+        // node = head;
+        // for (int i=0;i<n/2;i++) node=node->next;
+        // return node;
+
+        // one pass
+        ListNode *slow=head, *fast=head;
+        while (fast and fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
     }
 };

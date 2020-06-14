72fe0b45ad02cd9ac50bf885268b64d116954202
ren <ryanzjlib@gmail.com>
2020-06-14 15:00:00 -0400

    add 50

---
+++ code/50_solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long N = (long)n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double accum = 1;
+        while (N) {
+            if (N & 1) {
+                accum *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+        return accum;
+    }
+};

76441a48e0550fb3d32662fcdcda47eb90a99445
Ren Zhang <ryanzjlib@gmail.com>
2020-07-16 14:44:32 -0400

    add 50

--- code/50_solution.py
+++ code/50_solution.py
@@ -1,12 +1,23 @@
+# 2^10 = 1*2*2*2*2*2*2*2*2*2*2 = 1*2^(1+1+1...+1)
+#                              = 1*2^(2+8)
+
+# 2^10 = 1* 2^2 * 2^8 
+
+#                8 4 2 1
+# bin(10)        1 0 1 0
+
+# 2^(-2) = 2^(-1*2) = (2^-1)*2
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        
         if n < 0:
             x = 1 / x
-            n = -n
+            n = - n
             
-        p = 1
+        accum, p = 1, x
         while n:
-            if n & 1: p *= x
-            x *= x
-            n //= 2
-        return p
+            if n & 1: accum *= p
+            p *= p
+            n >>= 1
+        return accum

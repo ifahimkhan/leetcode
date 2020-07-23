c3d4c902edfbf820937115fee4b7bbb4183e7cc2
Ren Zhang <ryanzjlib@gmail.com>
2020-07-23 16:12:15 -0400

    Create 1089_solution.py

---
+++ code/1089_solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        zeroes = arr.count(0)
+        n = len(arr)
+        for i in range(n-1, -1, -1):
+            if i + zeroes < n: arr[i + zeroes] = arr[i]
+            if arr[i]: continue
+            zeroes -= 1
+            if i + zeroes < n: arr[i + zeroes] = arr[i]

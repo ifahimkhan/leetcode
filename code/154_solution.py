21015feccc7fdbae525a3a7484743d1a3e8ea2a1
Ren Zhang <ryanzjlib@gmail.com>
2020-07-25 15:07:51 -0400

    update 154 and list

--- code/154_solution.py
+++ code/154_solution.py
@@ -1,12 +1,10 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l, h = 0, len(nums)-1
+        # O(log(N)) when there is no duplicate, O(N) worst case, when we just have a single number repeating
+        l, h = 0, len(nums) - 1
         while l < h:
-            mid = (l + h) // 2
-            if nums[mid] < nums[h]:
-                h = mid 
-            elif nums[mid] > nums[h]:
-                l = mid + 1
-            else:
-                h -= 1
-        return nums[l]        
+            m = (l + h) // 2
+            if nums[m] > nums[h]: l = m + 1
+            elif nums[m] < nums[h]: h = m
+            else: h -= 1
+        return nums[l] # nums[h]

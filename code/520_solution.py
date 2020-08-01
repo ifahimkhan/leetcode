ac8a00520cc811d7de712771ca419b927e4b8f74
Ren Zhang <ryanzjlib@gmail.com>
2020-08-01 07:55:20 -0400

    add early break

--- code/520_solution.py
+++ code/520_solution.py
@@ -1,4 +1,12 @@
 class Solution:
-    def detectCapitalUse(self, word: str) -> bool:
+    def solution_1(self, word: str) -> bool:
         upper_count = sum(map(str.isupper, word))
-        return (upper_count % len(word) == 0) or (upper_count == 1 and word[0].isupper())
+        return upper_count % len(word) == 0 or (upper_count == 1 and word[0].isupper())
+    
+    
+    def solution_2(self, word):
+        if word[0].isupper():
+            return all(map(str.isupper, word)) or all(map(lambda x: word[x].islower(), range(1, len(word))))
+        return all(map(str.islower, word))
+
+    detectCapitalUse = solution_2 # solution_1

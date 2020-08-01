072789fdd3d691ca7fe2968760505538b6e12369
Ren Zhang <ryanzjlib@gmail.com>
2020-08-01 11:22:40 -0400

    Update 520_solution.py

--- code/520_solution.py
+++ code/520_solution.py
@@ -8,5 +8,15 @@ class Solution:
         if word[0].isupper():
             return all(map(str.isupper, word)) or all(map(lambda x: word[x].islower(), range(1, len(word))))
         return all(map(str.islower, word))
-
-    detectCapitalUse = solution_2 # solution_1
+    
+    def solution_3(self, word):
+        first_cap = word[0].isupper()
+        c = 0
+        for i, char in enumerate(word, 1):
+            c += char.isupper()
+            if not c: continue
+            if first_cap and c in [1, i]: continue
+            return False
+        return True
+    
+    detectCapitalUse = solution_3 # solution_2 # solution_1

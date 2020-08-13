488fe6582be511e08b8839cf4618853be451993b
ren <ryanzjlib@gmail.com>
2020-08-12 21:48:42 -0400

    update 235

--- code/235_solution.py
+++ code/235_solution.py
@@ -6,10 +6,19 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root: return
+    def recursive(self, root, p, q):
+        if not root: return root
         if p.val < root.val > q.val:
             return self.lowestCommonAncestor(root.left, p, q)        
         if p.val > root.val < q.val:
             return self.lowestCommonAncestor(root.right, p, q)        
         return root
+
+    def iterative(self, root, p, q):
+        node = root
+        while node:
+            if p.val > node.val and q.val > node.val: node = node.right
+            elif p.val < node.val and q.val < node.val: node = node.left
+            else: return node
+
+    lowestCommonAncestor = iterative # recursive

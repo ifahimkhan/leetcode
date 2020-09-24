7eb9e9a3139b0d7631ee92e87b94576b1db9bddb
Ren Zhang <ryanzjlib@gmail.com>
2020-09-24 09:07:19 -0400

    Create 389_solution.cpp

---
+++ code/389_solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char a=0;
+        for (char c:s) a^=c;
+        for (char c:t) a^=c;
+        return a;
+    }
+};

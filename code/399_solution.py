809b2768c14c9b53a4e82194b3d7feeb1a421e61
Ren Zhang <ryanzjlib@gmail.com>
2020-09-27 08:49:44 -0400

    Create 399_solution.py

---
+++ code/399_solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        # build graph
+        edges = defaultdict(defaultdict)
+        for (num, denum), value in zip(equations, values):
+            edges[num][denum] = value
+            edges[denum][num] = 1 / value
+
+        for num in edges.keys(): edges[num][num] = 1
+        
+        def dfs(current, target, used):
+            if current not in edges: return -1
+            if target in edges[current]: return edges[current][target]
+            
+            used.add(current)
+            for next_ in filter(lambda x: x not in used, edges[current]):
+                result = dfs(next_, target, used)
+                if result != -1: return edges[current][next_] * result
+            return -1
+        
+        return [dfs(*query, set()) for query in queries]

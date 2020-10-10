e150ad3afa48568d0ca28a5d2e4ed73468aebc7c
Ren Zhang <ryanzjlib@gmail.com>
2020-10-10 08:03:26 -0400

    Create 452_solution.py

---
+++ code/452_solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        ret, shoot = 0, float('inf')
+        for s, e in sorted(points, reverse=True):
+            if shoot > e:
+                shoot = s
+                ret += 1
+        return ret
+        

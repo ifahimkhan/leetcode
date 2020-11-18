4a31ba6355d9e37694fef1eba66a7e298451c223
Ren Zhang <ryanzjlib@gmail.com>
2020-11-18 07:20:50 -0500

    Create 193_solution.sh

---
+++ code/193_solution.sh
@@ -0,0 +1,5 @@
+# Read from the file file.txt and output all valid phone numbers to stdout.
+
+sed -n -r '/^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/p' file.txt
+
+# grep -E '^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$' file.txt

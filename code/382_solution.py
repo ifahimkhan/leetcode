f2d3bf2ec1491932df721291ba74ed2fb41b8023
Ren Zhang <ryanzjlib@gmail.com>
2020-12-02 07:19:34 -0500

    Update 382_solution.py

--- code/382_solution.py
+++ code/382_solution.py
@@ -12,24 +12,25 @@ class Solution:
         @param head The linked list's head.
         Note that the head is guaranteed to be not null, so it contains at least one node.
         """
-        # self.head = head
-        # node = head
-        # self.count = 0
-        # while node:
-        #     self.count += 1
-        #     node = node.next
         self.head = head
 
-    def getRandom(self) -> int:
-        """
-        Returns a random node's value.
-        """
-        # node = self.head
-        # i = random.randint(0, self.count - 1)
-        # for j in range(i):
-        #     node = node.next
-        # return node.val
+    def count(self):
+        node = self.head
+        num_nodes = 0
+        while node:
+            num_nodes += 1
+            node = node.next
+        return num_nodes
         
+    def count_then_get(self):
+        num_nodes = self.count()
+        i = random.randint(1, num_nodes)
+        node = self.head
+        for _ in range(i):
+            node = node.next
+        return node.val        
+    
+    def reservoir_sampling(self):
         cnt = 0
         curr = self.head
         
@@ -39,6 +40,8 @@ class Solution:
             curr = curr.next
             cnt += 1
         return val
+    
+    getRandom = reservoir_sampling # count_then_get
                 
 
 

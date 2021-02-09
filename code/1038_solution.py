7d8aac44ecc938a13f992fcb4ac3b8c47c217922
Ren Zhang <ryanzjlib@gmail.com>
2021-02-09 09:07:23 -0500

    Create 1038_solution.py

---
+++ code/1038_solution.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    total = 0    
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        if root is not None:
+            self.bstToGst(root.right)
+            self.total += root.val
+            root.val = self.total
+            self.bstToGst(root.left)
+        return root        

ad2fa97f94075d016b315a16a639c9dfe1e80bf5
Ren Zhang <ryanzjlib@gmail.com>
2021-02-17 06:42:48 -0500

    Create 11_solution.py

---
+++ code/11_solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        max_area = min(height[l], height[r]) * (r - l)
+        
+        while l != r:
+            if height[l] <= height[r]: l += 1
+            else: r -= 1
+            max_area = max(max_area, min(height[l], height[r]) * (r - l))
+
+        return max_area

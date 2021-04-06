b746611829487123ffc8537a7bec1c76332ba0c6
Ren Zhang <ryanzjlib@gmail.com>
2021-04-06 11:03:41 -0400

    Update 1249_solution.py

--- code/1249_solution.py
+++ code/1249_solution.py
@@ -1,9 +1,24 @@
 class Solution:
-    def minRemoveToMakeValid(self, s: str) -> str:
+    def solution_stack_and_set(self, s):
+        stack = [] # keep track of unclosed open parentheses
+        to_delete = set() # keep track of indices to delete
+        for i, char in enumerate(s):  # first pass
+            if char == '(':
+                stack.append(i)  
+            if char == ')':
+                if not stack:
+                    to_delete.add(i)
+                else:
+                    stack.pop()
+        to_delete.update(stack)
+        return ''.join([char for i, char in enumerate(s) if i not in to_delete]) # second pass
+    
+    
+    def solution_stack_only(self, s: str) -> str:
         result = []
-        capacity = s.count(')')
+        capacity = s.count(')')  # first pass
         opened = 0
-        for char in s:
+        for char in s:  # second pass
             if char == '(':
                 if opened == capacity: continue              
                 opened += 1
@@ -12,5 +27,6 @@ class Solution:
                 if not opened: continue
                 opened -= 1
             result.append(char)
-        return ''.join(result)
-        
+        return ''.join(result) # third pass
+       
+    minRemoveToMakeValid = solution_stack_and_set # solution_stack_only

6fb1fc70f8c807d59e056ab8fb1aa5cfca93cb3c
Ren Zhang <ryanzjlib@gmail.com>
2021-04-06 12:01:13 -0400

    Update 953_solution.py
    Could be a follow up

--- code/953_solution.py
+++ code/953_solution.py
@@ -11,3 +11,26 @@ class Solution:
         for w1, w2 in zip(words, words[1:]):
             if not lex_order(w1, w2): return False
         return True
+        
+class AlienSort:
+    def __init__(self, order):
+        self.order = {char: i for i, char in enumerate(order)}
+        self._init_key_func()
+
+    def _init_key_func(self):
+        def lexi_order(word1, word2):
+            for c1, c2 in zip(word1, word2):
+                if c1 == c2: continue
+                return self.order[c1] - self.order[c2]
+            return len(word1) - len(word2)
+        self.key_func = __import__('functools').cmp_to_key(lexi_order)
+        
+    def sort(self, words):
+        words.sort(key=self.key_func)
+
+
+if __name__ == '__main__':
+    words = ["hello","leetcode", "bla", "wach", "lhhh"]
+    order = "hlabcdefgijkmnopqrstuvwxyz"
+    AlienSort(order).sort(words)
+    print(words    

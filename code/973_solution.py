5d99b0caafaedc5dc07e64742381c1b25a483976
Ren Zhang <ryanzjlib@gmail.com>
2021-04-06 20:44:06 -0400

    Update 973_solution.py

--- code/973_solution.py
+++ code/973_solution.py
@@ -11,9 +11,10 @@ class Solution:
 #             elif d < -pq[0][0]:
 #                 heapq.heapreplace(pq, (-d, p))
 #         return [t[1] for t in pq]
+        
         @lru_cache(maxsize=len(points))
-        def dist(x, y):
-            return x ** 2 + y ** 2            
+        def dist(loc):
+            return loc[0] ** 2 + loc[1] ** 2
         
         def swap(i, j):
             points[i], points[j] = points[j], points[i]
@@ -24,18 +25,18 @@ class Solution:
             swap(l, m)
             i, j = l, r
             while i < j:
-                while i < r and dist(*points[i]) <= pivot: i += 1
-                while j > l and dist(*points[j]) >= pivot: j -= 1
-                if i < j and dist(*points[i]) > dist(*points[j]): swap(i, j)
+                while i < r and dist(points[i]) <= pivot: i += 1
+                while j > l and dist(points[j]) >= pivot: j -= 1
+                if i < j and dist(points[i]) > dist(points[j]): swap(i, j)
             swap(j, l)
             return j
     
-        def sort(l, r, k):
+        def quickselect(l, r, k):
             m = partition(l, r)
             d = m - l + 1
             if d == k: return
             elif d < k: sort(m + 1, r, k - d)
             else: sort(l, m - 1, k) 
         
-        sort(0, len(points) - 1, K)
+        quickselect(0, len(points) - 1, K)
         return points[:K]

5bf2e4cc045b254c26f3983f5974f131597334a2
Ren Zhang <ryanzjlib@gmail.com>
2021-04-08 22:21:03 -0400

    Update 43_solution.py

--- code/43_solution.py
+++ code/43_solution.py
@@ -1,19 +1,27 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+
+        stoi = {s:i for i, s in enumerate('0123456789')}
+        itos = {i:s for i, s in enumerate('0123456789')}
+        
+        def _int(x): return stoi[x]
+            
+        def _str(x): return itos[x]
+        
         if num1 == '0' or num2 == '0': return '0'
-        
+
         l1, l2 = len(num1), len(num2)
-        partials = [0] * (l1 + l2)
-        
-        for i, d1 in enumerate(map(int, reversed(num1))):
-            for j, d2 in enumerate(map(int, reversed(num2))):
+        results = [0] * (l1 + l2)
+
+        for i, d1 in enumerate(map(_int, reversed(num1))):
+            for j, d2 in enumerate(map(_int, reversed(num2))):
                 carry, reminder = divmod(d1 * d2, 10)            
-                partials[i + j] += reminder
-                partials[i + j + 1] += carry
-        
-        for i, partial in enumerate(partials):
-            carry, reminder = divmod(partial, 10)
-            partials[i] = reminder
-            if carry: partials[i + 1] += carry
-        
-        return ''.join(map(str, reversed(partials))).lstrip('0')
+                results[i + j] += reminder
+                results[i + j + 1] += carry
+
+        for i, d in enumerate(results):
+            carry, reminder = divmod(d, 10)
+            results[i] = reminder
+            if carry: results[i + 1] += carry
+
+        return ''.join(map(_str, reversed(results))).lstrip('0')

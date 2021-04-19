1cb87bb00676aecc23a038779b4f8c63156c25fa
Ren Zhang <ryanzjlib@gmail.com>
2021-04-19 12:13:28 -0400

    Update 1676_solution.py

--- code/1676_solution.py
+++ code/1676_solution.py
@@ -6,7 +6,7 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
+    def solution_dfs_handle_missing_nodes(root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
         total = len(nodes)
         nodes = set(nodes)
         def dfs(node):
@@ -17,3 +17,13 @@ class Solution:
             lca = node if n_found == total else None
             return lca, n_found
         return dfs(root)[0]        
+
+    def solution_dfs_doesnot(root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
+        nodes = set(nodes)
+        def dfs(node):
+            if not node or node in nodes: return node
+            lca_l, lca_r = dfs(node.left), dfs(node.right)
+            return node if lca_l and lca_r else lca_r or lca_l
+        return dfs(root)
+
+    lowestCommonAncestor = solution_dfs_doesnot # solution_dfs_handle_missing_nodes

5da0553326e198bd287d10828cdb14348e635b8e
Ren Zhang <ryanzjlib@gmail.com>
2021-04-19 17:42:49 -0400

    Update 543_solution.py

--- code/543_solution.py
+++ code/543_solution.py
@@ -5,16 +5,31 @@
 #         self.left = None
 #         self.right = None
 
+# Bascially, the same as 124
+
 class Solution:
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+    def solution_recursive(root: TreeNode) -> int:
+        max_diam[0] = 0
+
+        def dfs(node):
+            if not node: return 0
+            l = dfs(node.left)
+            r = dfs(node.right)
+            max_diam[0] = max(max_diam[0], l+r)
+            return max(0, 1 + max(l,r))
+
+        dfs(root)
+        return max_diam[0]        
+
+    def solution_iterative(root: TreeNode) -> int:
         max_dim = 0
-        depths = collections.defaultdict(int)
+        depths = defaultdict(int)
         stack = [root] if root else []
         while stack:
             node = stack[-1]
-            if node.left and node.left not in depths: 
+            if node.left and node.left not in depths:
                 stack.append(node.left)
-            elif node.right and node.right not in depths: 
+            elif node.right and node.right not in depths:
                 stack.append(node.right)
             else:
                 stack.pop()
@@ -22,5 +37,5 @@ class Solution:
                 depths[node] = 1 + max(l, r)
                 max_dim = max(max_dim, l + r)
         return max_dim
-                
     
+    diameterOfBinaryTree = solution_iterative

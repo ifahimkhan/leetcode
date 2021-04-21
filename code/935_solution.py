b5f406b706db804c23e59232125c9bcde272a2ee
Ren Zhang <ryanzjlib@gmail.com>
2021-04-21 10:28:43 -0400

    Update 935_solution.py

--- code/935_solution.py
+++ code/935_solution.py
@@ -1,5 +1,8 @@
+MOD = 10 ** 9 + 7
+import numpy as np
+
 class Solution:
-    def knightDialer(self, n: int) -> int:
+    def solution_dp(self, n: int) -> int:
         jumps = {
             0: (4, 6),
             1: (6, 8),
@@ -19,5 +22,33 @@ class Solution:
             for k, v in dp.items():
                 for target in jumps[k]:
                     ndp[target] += v
+            for k in ndp:
+                ndp[k] = ndp[k] % MOD
             dp = ndp
-        return sum(dp.values()) % (10 ** 9 + 7)
+        return sum(dp.values()) % MOD
+
+    def solution_matrix_power(self, n):
+        if n == 1: return 10
+        
+        M = np.matrix([
+            # 0 1 2 3 4 5 6 7 8 9
+             [0,0,0,0,1,0,1,0,0,0],
+             [0,0,0,0,0,0,1,0,1,0],
+             [0,0,0,0,0,0,0,1,0,1],
+             [0,0,0,0,1,0,0,0,1,0],
+             [1,0,0,1,0,0,0,0,0,1],
+             [0,0,0,0,0,0,0,0,0,0],
+             [1,1,0,0,0,0,0,1,0,0],
+             [0,0,1,0,0,0,1,0,0,0],
+             [0,1,0,1,0,0,0,0,0,0],
+             [0,0,1,0,1,0,0,0,0,0]
+        ])
+        dp, N = 1, n - 1
+        while N:
+            if N % 2: dp = dp * M % MOD
+            M = M * M % MOD
+            N //= 2
+        return int(np.sum(dp)) % MOD
+    
+    knightDialer = solution_matrix_power # solution_dp
+            
